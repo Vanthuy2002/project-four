643142c32813ebdbd2e5a10107e8427386d48f4d
Thuy Nguyen <vanthuy101102@gmail.com>
2023-05-07 10:43:57 +0700

    lam phan blog

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,47 @@
 const boardService = {
-  monthly: [{ price: "$99" }, { price: "$150" }, { price: "$299" }],
-  yearly: [{ price: "$199" }, { price: "$250" }, { price: "$399" }],
+  monthly: [{ price: '$99' }, { price: '$150' }, { price: '$299' }],
+  yearly: [{ price: '$199' }, { price: '$250' }, { price: '$399' }],
 };
 
 const comments = [
   {
-    img: "./assets/img/pet-cmt1.jpg",
-    auth: "Thuy Nguyen",
+    img: './assets/img/pet-cmt1.jpg',
+    auth: 'Thuy Nguyen',
     title:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit ea a nulla eaque dicta, labore nisi, recusandae dolores laboriosam impedit nemo ex asperiores illo tempore. Perferendis odit perspiciatis quia sapiente?",
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit ea a nulla eaque dicta, labore nisi, recusandae dolores laboriosam impedit nemo ex asperiores illo tempore. Perferendis odit perspiciatis quia sapiente?',
   },
 
   {
-    img: "./assets/img/pet-cmt2.jpg",
-    auth: "Yua Mikami",
+    img: './assets/img/pet-cmt2.jpg',
+    auth: 'Yua Mikami',
     title:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit ea a nulla eaque dicta, labore nisi, recusandae dolores???",
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit ea a nulla eaque dicta, labore nisi, recusandae dolores???',
   },
 
   {
-    img: "./assets/img/pet-cmt3.jpg",
-    auth: "Emi Fukuda",
+    img: './assets/img/pet-cmt3.jpg',
+    auth: 'Emi Fukuda',
     title:
-      "Aliquam repudiandae molestias, ipsum sit voluptatem doloribus explicabo odio, ducimus dolores officiis dolorem enim doloremque voluptates reiciendis commodi autem consectetur omnis soluta recusandae vero??",
+      'Aliquam repudiandae molestias, ipsum sit voluptatem doloribus explicabo odio, ducimus dolores officiis dolorem enim doloremque voluptates reiciendis commodi autem consectetur omnis soluta recusandae vero??',
   },
   {
-    img: "./assets/img/pet-cmt4.jpg",
-    auth: "Richad Conner",
+    img: './assets/img/pet-cmt4.jpg',
+    auth: 'Richad Conner',
     title:
-      "Maiores enim ipsa aut, aspernatur aliquam eligendi libero sapiente debitis ducimus consectetur, sequi provident sit culpa molestiae fugit cumque cum deserunt?",
+      'Maiores enim ipsa aut, aspernatur aliquam eligendi libero sapiente debitis ducimus consectetur, sequi provident sit culpa molestiae fugit cumque cum deserunt?',
   },
 ];
 
-let navLinks = document.querySelectorAll(".header__link");
-let btns = document.querySelectorAll(".btn");
-let tabItems = document.querySelectorAll(".tab__item");
-let pricingSpinner = document.querySelectorAll(".pricing__spinner");
-let pricingList = document.querySelector(".pricing__list");
-let pricingNum = document.querySelectorAll(".pricing__number");
-let reviewsContents = document.querySelector(".reviews__contents");
-let listReview = document.querySelector(".reviews__list");
-let next = document.querySelector(".next");
-let prev = document.querySelector(".prev");
+let navLinks = document.querySelectorAll('.header__link');
+let btns = document.querySelectorAll('.btn');
+let tabItems = document.querySelectorAll('.tab__item');
+let pricingSpinner = document.querySelectorAll('.pricing__spinner');
+let pricingList = document.querySelector('.pricing__list');
+let pricingNum = document.querySelectorAll('.pricing__number');
+let reviewsContents = document.querySelector('.reviews__contents');
+let listReview = document.querySelector('.reviews__list');
+let next = document.querySelector('.next');
+let prev = document.querySelector('.prev');
 
 function renderReviews(arrCmt) {
   let itemReviews = arrCmt.map((cmt) => {
@@ -64,18 +64,19 @@ function renderReviews(arrCmt) {
     </section>
   </article>`;
   });
-  listReview.innerHTML = itemReviews.join("");
+  listReview.innerHTML = itemReviews.join('');
 }
 
 renderReviews(comments);
 let index = 0;
-let items = document.querySelectorAll(".reviews__item");
+let items = document.querySelectorAll('.reviews__item');
 
 next.onclick = () => {
   index++;
-  handleClass(next, "add", "active-controls");
-  if (index >= items.length) {
-    handleClass(next, "remove", "active-controls");
+  handleClass(next, 'add', 'active-controls');
+  if (index >= items.length - 1) {
+    handleClass(next, 'remove', 'active-controls');
+  } else {
   }
   loadComments();
 };
@@ -92,34 +93,34 @@ function loadPrevCmt() {
 
 prev.onclick = () => {
   index--;
-  handleClass(prev, "add", "active-controls");
+  handleClass(prev, 'add', 'active-controls');
   if (index === 0) {
-    handleClass(prev, "remove", "active-controls");
+    handleClass(prev, 'remove', 'active-controls');
   }
   loadPrevCmt();
 };
 
 btns.forEach((btn) => {
   btn.onclick = () => {
-    [...btns].forEach((item) => handleClass(item, "remove", "active"));
-    handleClass(btn, "add", "active");
+    [...btns].forEach((item) => handleClass(item, 'remove', 'active'));
+    handleClass(btn, 'add', 'active');
   };
 });
 
 navLinks.forEach((link) => {
   link.onclick = () => {
-    [...navLinks].forEach((item) => handleClass(item, "remove", "active-link"));
-    handleClass(link, "add", "active-link");
+    [...navLinks].forEach((item) => handleClass(item, 'remove', 'active-link'));
+    handleClass(link, 'add', 'active-link');
   };
 });
 
 tabItems.forEach((tab) => {
   tab.onclick = () => {
     [...tabItems].forEach((item) =>
-      handleClass(item, "remove", "tab__item-active")
+      handleClass(item, 'remove', 'tab__item-active')
     );
 
-    handleClass(tab, "add", "tab__item-active");
+    handleClass(tab, 'add', 'tab__item-active');
   };
 });
 
@@ -131,9 +132,9 @@ pricingSpinner.forEach(
   (item, index) =>
     (item.onclick = () => {
       [...pricingSpinner].forEach((el) =>
-        handleClass(el, "remove", "actively")
+        handleClass(el, 'remove', 'actively')
       );
-      handleClass(item, "add", "actively");
+      handleClass(item, 'add', 'actively');
 
       if (index === 0) {
         handleAddPrice(boardService.monthly);
